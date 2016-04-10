07d73dc823df57f67c8608377f6134334e601058
Rodrigo Otero <rodri.m3n@gmail.com>
2016-04-10 17:43:10 -0300

    fafa

--- src/main/java/bd2/model/Prueba.java
+++ src/main/java/bd2/model/Prueba.java
@@ -1,34 +1,75 @@
 package bd2.model;
 
 /**
- * @author
+ * The Class Prueba.
+ *
+ * @author 
  */
 public class Prueba {
+    
+    /** The puntaje. */
     protected int puntaje;
+    
+    /** The leccion. */
     protected Leccion leccion;
+    
+    /** The id. */
     private long id;
 
+    /**
+     * Instantiates a new prueba.
+     *
+     * @param leccion the leccion
+     * @param puntaje the puntaje
+     * @throws Exception the exception
+     */
     public Prueba(Leccion leccion, Integer puntaje) throws Exception {
         this.setLeccion(leccion);
         this.setPuntaje(puntaje);
     }
 
+    /**
+     * Instantiates a new prueba.
+     *
+     * @throws Exception the exception
+     */
     public Prueba() throws Exception {
         super();
     }
 
+    /**
+     * Gets the id.
+     *
+     * @return the id
+     */
     public long getId() {
         return this.id;
     }
 
+    /**
+     * Sets the id.
+     *
+     * @param id the new id
+     */
     public void setId(long id) {
         this.id = id;
     }
 
+    /**
+     * Gets the puntaje.
+     *
+     * @return the puntaje
+     */
     public int getPuntaje() {
         return this.puntaje;
     }
 
+    /**
+     * Sets the puntaje.
+     *
+     * @param puntaje the new puntaje
+     * @throws Exception the exception
+     */
     public void setPuntaje(int puntaje) throws Exception {
         if (puntaje < 0)
             throw new Exception(
@@ -38,14 +79,29 @@ public class Prueba {
         this.puntaje = puntaje;
     }
 
+    /**
+     * Gets the leccion.
+     *
+     * @return the leccion
+     */
     public Leccion getLeccion() {
         return this.leccion;
     }
 
+    /**
+     * Sets the leccion.
+     *
+     * @param leccion the new leccion
+     */
     public void setLeccion(Leccion leccion) {
         this.leccion = leccion;
     }
 
+    /**
+     * Aprobada.
+     *
+     * @return the boolean
+     */
     public Boolean aprobada() {
         return this.getPuntaje() >= 60;
     }
--- src/main/java/bd2/model/Sitio.java
+++ src/main/java/bd2/model/Sitio.java
@@ -3,62 +3,89 @@ package bd2.model;
 import java.util.Collection;
 import java.util.HashSet;
 
-
 /**
+ * The Class Sitio.
+ *
  * @author farfanoide
  */
 public class Sitio {
 
-    private Collection<Usuario> usuarios = new HashSet<Usuario>();
-    private Collection<Curso> cursos = new HashSet<Curso>();
-    private Collection<Documento> documentos = new HashSet<Documento>();
-    private long id;
+	/**
+	 * Los usuarios. Utilizamos un set para no tener usuarios duplicados
+	 */
+	private Collection<Usuario> usuarios;
 
-    public long getId() {
-        return this.id;
-    }
+	/**
+	 * Los cursos. Idem usuarios.
+	 */
+	private Collection<Curso> cursos;
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	/** The documentos. */
+	private Collection<Documento> documentos;
 
-    public Sitio() {
-    }
+	/**
+	 * Instantiates a new sitio.
+	 */
+	public Sitio() {
+		this.usuarios = new HashSet<Usuario>();
+		this.cursos = new HashSet<Curso>();
+		this.documentos = new HashSet<Documento>();
+	}
 
-    public Collection<Usuario> getUsuarios() {
-        return this.usuarios;
-    }
+	/**
+	 * Gets the usuarios.
+	 *
+	 * @return the usuarios
+	 */
+	public Collection<Usuario> getUsuarios() {
+		return this.usuarios;
+	}
 
-    public void registrarUsuario(Usuario usuario) {
-        this.getUsuarios().add(usuario);
-    }
+	/**
+	 * Registrar usuario.
+	 *
+	 * @param usuario
+	 *            the usuario
+	 */
+	public void registrarUsuario(Usuario usuario) {
+		this.getUsuarios().add(usuario);
+	}
 
-    public Collection<Curso> getCursos() {
-        return this.cursos;
-    }
+	/**
+	 * Gets the cursos.
+	 *
+	 * @return the cursos
+	 */
+	public Collection<Curso> getCursos() {
+		return this.cursos;
+	}
 
-    public void agregarCurso(Curso curso) {
-        this.getCursos().add(curso);
-    }
+	/**
+	 * Agregar curso.
+	 *
+	 * @param curso
+	 *            the curso
+	 */
+	public void agregarCurso(Curso curso) {
+		this.getCursos().add(curso);
+	}
 
-    public void agregarDocumento(Documento documento) {
-        this.getDocumentos().add(documento);
-    }
+	/**
+	 * Agregar documento.
+	 *
+	 * @param documento
+	 *            the documento
+	 */
+	public void agregarDocumento(Documento documento) {
+		this.getDocumentos().add(documento);
+	}
 
-    public void setUsuarios(Collection<Usuario> usuarios) {
-        this.usuarios = usuarios;
-    }
-
-    public void setCursos(Collection<Curso> cursos) {
-        this.cursos = cursos;
-    }
-
-    public Collection<Documento> getDocumentos() {
-        return this.documentos;
-    }
-
-    public void setDocumentos(Collection<Documento> documentos) {
-        this.documentos = documentos;
-    }
-
+	/**
+	 * Gets the documentos.
+	 *
+	 * @return the documentos
+	 */
+	public Collection<Documento> getDocumentos() {
+		return this.documentos;
+	}
 }
